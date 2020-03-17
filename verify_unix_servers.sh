1ff1a632163923cb25a83f9d419ca8820f61a377
Ravindra Reddy <Ravindra.Reddy@JohnLewis.co.uk>
2020-03-17 17:12:28 +0000

    verify_unix_servers.sh edited online with Bitbucket

--- verify_unix_servers.sh
+++ verify_unix_servers.sh
@@ -3,7 +3,7 @@
 date
 cat servers_list.txt |  while read output
 do
-    ping -c 1 output > /dev/null
+    ping -c 1 $output
     if [ $? -eq 0 ]; then
     echo "Server :$output is Reachable"
     else

2d5fbea8319d26bfc987e676ed37504563d9a491
Ravindra Reddy <ravindra.reddy@johnlewis.co.uk>
2020-04-03 06:24:46 +0100

    Added SYS and WMSYS in where condition

--- roles/invalid_objects/templates/all_objects_cmp_body_not_pub.sql
+++ roles/invalid_objects/templates/all_objects_cmp_body_not_pub.sql
@@ -1,7 +1,7 @@
 spool /home/oretail/pre_check_scripts/rc_all_objects_cmp_body_not_pub.sql
 select 'ALTER '|| OBJECT_TYPE || ' ' || OWNER || '.' || OBJECT_NAME || ' COMPILE BODY;'
  from all_objects dob
- where dob.owner <> 'PUBLIC'
+ where dob.owner not in ('PUBLIC','SYS','WMSYS')
  and dob.object_type in ('PACKAGE')
  and dob.GENERATED != 'Y'
  and dob.status='INVALID';
--- roles/invalid_objects/templates/dba_objects_cmp_body_not_pub.sql
+++ roles/invalid_objects/templates/dba_objects_cmp_body_not_pub.sql
@@ -1,7 +1,7 @@
 spool /home/oretail/pre_check_scripts/rc_dba_objects_cmp_body_not_pub.sql
 select 'ALTER '|| OBJECT_TYPE || ' ' || OWNER || '.' || OBJECT_NAME || ' COMPILE BODY;'
  from dba_objects dob
- where dob.owner <> 'PUBLIC'
+ where dob.owner not in ('PUBLIC','SYS','WMSYS')
  and dob.object_type in ('PACKAGE')
  and dob.GENERATED != 'Y'
  and dob.status='INVALID';

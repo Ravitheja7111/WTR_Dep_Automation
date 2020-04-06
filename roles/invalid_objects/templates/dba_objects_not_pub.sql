e0ee9f4413986422f680365681872318d86bc36d
Ravindra Reddy <Ravindra.Reddy@JohnLewis.co.uk>
2020-04-06 08:49:20 +0100

    dba_objects_not_pub.sql edited online with Bitbucket

--- roles/invalid_objects/templates/dba_objects_not_pub.sql
+++ roles/invalid_objects/templates/dba_objects_not_pub.sql
@@ -1,9 +1,9 @@
 spool /home/oretail/pre_check_scripts/rc_dba_objects_not_pub.sql
-select 'ALTER '||'OBJECT_TYPE'||OWNER||'.'||OBJECT_NAME||' COMPILE';
-  from dba_objects dob
-  where dob.owner <> 'PUBLIC'
-  and dob.object_type in ('SYNONYM','MATERIALIZED VIEW')
-  and dob.GENERATED != 'Y'
-  and dob.status='INVALID';
+select 'ALTER '||OBJECT_TYPE||' '||OWNER||'.'||OBJECT_NAME||' COMPILE;' 
+from dba_objects dob 
+where dob.owner <> 'PUBLIC' 
+and dob.object_type in ('SYNONYM','MATERIALIZED VIEW') 
+and dob.GENERATED != 'Y' 
+and dob.status='INVALID';
 spool off;
 @/home/oretail/pre_check_scripts/rc_dba_objects_not_pub.sql

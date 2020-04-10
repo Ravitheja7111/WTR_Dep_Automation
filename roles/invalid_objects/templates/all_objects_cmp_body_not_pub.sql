e995026e2b11342f8be1b010e61020a576a00e96
Ravindra Reddy <ravindra.reddy@johnlewis.co.uk>
2020-04-10 14:30:14 +0100

    Added momstage condition

--- roles/invalid_objects/templates/all_objects_cmp_body_not_pub.sql
+++ roles/invalid_objects/templates/all_objects_cmp_body_not_pub.sql
@@ -1,7 +1,7 @@
 spool /home/oretail/pre_check_scripts/rc_all_objects_cmp_body_not_pub.sql
 select 'ALTER '|| OBJECT_TYPE || ' ' || OWNER || '.' || OBJECT_NAME || ' COMPILE BODY;'
  from all_objects dob
- where dob.owner not in ('PUBLIC','SYS','WMSYS')
+ where dob.owner not in ('PUBLIC','SYS','WMSYS','MOMSTAGE')
  and dob.object_type in ('PACKAGE')
  and dob.GENERATED != 'Y'
  and dob.status='INVALID';
--- roles/invalid_objects/templates/dba_objects_cmp_pkg_not_pub.sql
+++ roles/invalid_objects/templates/dba_objects_cmp_pkg_not_pub.sql
@@ -1,7 +1,7 @@
 spool /home/oretail/pre_check_scripts/rc_dba_objects_cmp_pkg_not_pub.sql
 select 'ALTER '|| OBJECT_TYPE || ' ' || OWNER || '.' || OBJECT_NAME || ' COMPILE PACKAGE;'
  from dba_objects dob
- where dob.owner not in ('PUBLIC','SYS','WMSYS')
+ where dob.owner not in ('PUBLIC','SYS','WMSYS','MOMSTAGE')
  and dob.object_type in ('PACKAGE')
  and dob.GENERATED != 'Y'
  and dob.status='INVALID';

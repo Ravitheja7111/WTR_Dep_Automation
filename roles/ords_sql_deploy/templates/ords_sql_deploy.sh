21ec40cee34e143259bdea708760071732a27876
Ravindra Reddy <ravindra.reddy@johnlewis.co.uk>
2020-04-28 11:24:12 +0100

    Added ORDS role scripts

---
+++ roles/ords_sql_deploy/templates/ords_sql_deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+source /home/oretail/.profile
+source /home/oretail/batch.profile
+VAR=`cat /home/oretail/deploy_list.txt`
+echo "var:$VAR"
+echo "line:"
+log_file_name=output_`date +%Y-%m-%d`.log
+log_time=_`date +%X`
+if [ -f "$log_file_name" ]; then
+ `mv $log_file_name "$log_file_name$log_time"`
+fi
+while IFS= read -r line; do
+    echo "Line: $line"
+    file_name=${line%~*}
+    echo "file_name:$file_name"
+    schema=${line#*~}
+    echo "schema:$schema"
+    if [ "$schema" == "ORDS" ];then
+	   con_string="{{ ords_schema }}"
+	   else
+  		con_string="{{ ops_schema }}"
+    fi
+    echo $file_name
+    echo $con_string
+    echo "sqlplus $con_string << EOF >> $log_file_name"
+sqlplus $con_string << EOF >> $log_file_name
+@{{ Staging }}/$file_name;
+commit;
+exit;
+EOF
+RETVAL=`grep -E "unknown command|unable to open file|ERROR at|ORA-*|*error*" $log_file_name | wc -l`
+if [ $RETVAL -gt 0 ];then
+echo "1st SQLPLUS FAILED : $RETVAL"
+   exit 1
+fi 	
+done <<< "$VAR"

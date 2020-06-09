bc5919ceefaf9bcac18ec1225d435352fce678aa
Ravindra Reddy <Ravindra.Reddy@JohnLewis.co.uk>
2020-06-09 11:55:28 +0100

    Added conditions for mmb and pll files compilation

--- roles/forms_deploy/templates/forms_compilation.sh
+++ roles/forms_deploy/templates/forms_compilation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#source /home/rmsapp/.profile
 forms_file={{ form_file_path }}/forms.txt
 sed -i 's/\r//g' $forms_file
 
@@ -22,31 +22,82 @@ log_name=""$db_name"_$(date +"%Y-%m-%d-%T")"
 forms_dep_logs={{ staging_base }}/forms_dep_logs/$log_name
 mkdir -p $forms_dep_logs
 forms_dep_files={{ form_file_path }}
-#Reading each form and compiling
+
+########## FMB files compilation ##################
 for form_name in `cat "$forms_file"`
 do
-        form_log="$form_name".log
-        frm_name=`echo $form_name | cut -d '.' -f1`
+    form_log="$form_name".log
+    frm_name=`echo $form_name | cut -d '.' -f1`
+    frm_ext=`echo $form_name | cut -d '.' -f2`
+    if [ $frm_ext == fmb ];then
         cp -p $forms_dep_files/$form_name $ORACLE_PATH
-        chmod -R 777 $ORACLE_PATH/$form_name
+        chmod 777 $ORACLE_PATH/$form_name
         echo "copied $form_name to $ORACLE_PATH ========================================================="
-        $ORACLE_PATH/frmcmp_batch.sh userid=$UP batch=no module=$ORACLE_PATH/$form_name output_file=$ORACLE_PATH/"$frm_name".fmx  module_type=form compile_all=yes window_state=minimize
-    form_com="$frm_name".fmx
-    #Check if compiled form .fmx file available in src and ready to move to bin
-    if [ -e $ORACLE_PATH/$form_com ];then
-        echo "$form_com has been generated"
-        chmod -R 777 $ORACLE_PATH/$form_com
-        echo "Permission has been changed for $form_com" >> $forms_dep_logs/$form_log
-        mv $ORACLE_PATH/$form_com $ORACLE_PATH/../bin
-        cd $ORACLE_PATH/../bin
-        if [ -e $form_com ];then
-            echo "$form_com has been moved to bin" >> $forms_dep_logs/$form_log
+		$ORACLE_PATH/frmcmp_batch.sh userid=$UP batch=no module=$ORACLE_PATH/$form_name output_file=$ORACLE_PATH/"$frm_name".fmx  module_type=form compile_all=yes window_state=minimize
+        form_com="$frm_name".fmx
+		#Check if compiled form .fmx file available in src and ready to move to bin
+        if [ -e $ORACLE_PATH/$form_com ];then
+            echo "$form_com has been generated"
+            chmod -R 777 $ORACLE_PATH/$form_com
+            echo "Permission has been changed for $form_com" >> $forms_dep_logs/$form_log
+            mv $ORACLE_PATH/$form_com $ORACLE_PATH/../bin
+            cd $ORACLE_PATH/../bin
+            if [ -e $form_com ];then
+                echo "$form_com has been moved to bin" >> $forms_dep_logs/$form_log
+            else
+                echo "$form_com has not been moved to bin properly" >> $forms_dep_logs/$form_log
+            fi
         else
-            echo "$form_com has not been moved to bin properly" >> $forms_dep_logs/$form_log
+            echo "Form compilation has been failed " >> $forms_dep_logs/$form_log
+            exit -1
         fi
-    else
-        echo "Form compilation has been failed " >> $forms_dep_logs/$form_log
-        exit -1
+    elif [ $frm_ext == mmb ];then
+		form_log="$form_name".log
+		cp -p $forms_dep_files/$form_name $ORACLE_PATH
+		chmod 777 $ORACLE_PATH/$form_name
+		echo "copied $form_name to $ORACLE_PATH ========================================================="
+		$ORACLE_PATH/frmcmp_batch.sh userid=$UP batch=no module=$ORACLE_PATH/$form_name  output_file=$ORACLE_PATH/"$frm_name".mmx module_type=menu compile_all=yes window_state=minimize
+		form_com="$frm_name".mmx
+		#Check if compiled form .fmx file available in src and ready to move to bin
+		if [ -e $ORACLE_PATH/$form_com ];then
+			echo "$form_com has been generated"
+			chmod -R 777 $ORACLE_PATH/$form_com
+			echo "Permission has been changed for $form_com" >> $forms_dep_logs/$form_log
+			mv $ORACLE_PATH/$form_com $ORACLE_PATH/../bin
+			cd $ORACLE_PATH/../bin
+			if [ -e $form_com ];then
+					echo "$form_com has been moved to bin" >> $forms_dep_logs/$form_log
+			else
+					echo "$form_com has not been moved to bin properly" >> $forms_dep_logs/$form_log
+			fi
+		else
+			echo "Form compilation has been failed " >> $forms_dep_logs/$form_log
+			exit -1
+		fi
+        elif [ $frm_ext == pll ];then
+            form_log="$form_name".log
+            cp -p $forms_dep_files/$form_name $ORACLE_PATH
+            chmod 777 $ORACLE_PATH/$form_name
+            echo "copied $form_name to $ORACLE_PATH ========================================================="
+            $ORACLE_PATH/frmcmp_batch.sh userid=$UP batch=no module=$ORACLE_PATH/$form_name  output_file=$ORACLE_PATH/"$frm_name".plx module_type=library compile_all=yes window_state=minimize
+            form_com="$frm_name".plx
+            #Check if compiled form .fmx file available in src and ready to move to bin
+            if [ -e $ORACLE_PATH/$form_com ];then
+                echo "$form_com has been generated"
+                chmod -R 777 $ORACLE_PATH/$form_com
+                echo "Permission has been changed for $form_com" >> $forms_dep_logs/$form_log
+                mv $ORACLE_PATH/$form_com $ORACLE_PATH/../bin
+                cd $ORACLE_PATH/../bin
+                if [ -e $form_com ];then
+                    echo "$form_com has been moved to bin" >> $forms_dep_logs/$form_log
+                else
+                    echo "$form_com has not been moved to bin properly" >> $forms_dep_logs/$form_log
+                fi
+            else
+                echo "Form compilation has been failed " >> $forms_dep_logs/$form_log
+                exit -1
+            fi
+            else
+                echo "Form extensions are mis matched " >> $forms_dep_logs/$form_log
     fi
 done
-
